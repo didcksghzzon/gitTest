97fc9e07696be16b1341e4455f998677a4dc259b
양찬호 <@121.137.165.197>
2020-11-21 20:01:07 +0900

    t

--- test/src/testPackage/test.java
+++ test/src/testPackage/test.java
@@ -4,12 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Algorithm {
+public class test {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-		Algorithm algorithm = new Algorithm();
-		algorithm.test01();
+		test t = new test();
+		t.test01();
 
 	}
 	
